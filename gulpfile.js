dc3a6a29aad8fd5493133cf2a5240f3d74f4de08
MichelSimonot <Mitch.Simonot@kandy.io>
2016-09-15 08:41:31 -0400

    Create default task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,8 @@
 var gulp = require('gulp');
 
-var calculate = require('./src/calculator').calculate;
-//Run task; for easier testing for now.
-gulp.task('run', function(done) {
-    var equation = '2+3';
-    calculate(equation);
-    done();
-})
-
-
 // Add tasks to the gulpfile.
 require('require-dir')('./tasks');
+
+gulp.task('doSomething', function() {
+    console.log('I do something!');
+});
